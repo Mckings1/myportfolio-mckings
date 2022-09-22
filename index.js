e412bb584a4700638b98b8e07d6091ada3894580
oluwasegun michael <alabioluwasegun8@gmail.com>
2022-09-22 22:44:22 +0100

    js external

--- index.js
+++ index.js
@@ -20,7 +20,41 @@ const navbarlists = document.querySelector(".navbar-lists");
   navbarlists.classList.toggle("active");
 });  
 
+(function(){
+  emailjs.init("CMwa9tuiwZQLEtIqJ");
+})();
 
+var details = {
+name:username.value, phoneNumber:phoneNumber.value, email:email.value, message:msg.value
+};
+
+function sendEmail (){
+// e.preventDefault();
+// console.log(details)
+if (phoneNumber.value&&email.value&&msg.value&&username.value) {
+emailjs.send("service_xj5yhml","template_nqarsvg",{
+from_name: username.value,
+// to_name: "gff",
+message: msg.value,
+reply_to: "cgcbchc",
+phone_num:phoneNumber.value,
+email:email.value
+}).then(function(response) {
+ 
+
+   alert(`Hello ${username.value},\n Your Message was sent successfully. We Will get back to you!!!`)
+   username.value="";
+   msg.value="";
+   phoneNumber.value="";
+   email.value="";
+//    console.log('SUCCESS!',response, response.status, response.text);
+  }, function(error) {
+  alert("Hey! \n Internal server error")
+  });
+  }else{
+  alert("Please fill all the field")
+  }
+  }
 
 // var details = {
 //   name:username.value, phoneNumber:phoneNumber.value, email:email.value, message:msg.value
